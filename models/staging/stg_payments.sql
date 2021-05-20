655801331cc7e3073714febe88b9c5f0d7398213
Murat Kececi <mkececi@conaservices.com>
2021-05-20 15:42:27 +0000

    Updated stg_payments (amount divided by 100)

--- models/staging/stg_payments.sql
+++ models/staging/stg_payments.sql
@@ -3,7 +3,7 @@ select
     orderid as order_id,
     paymentmethod as payment_method,
     status as payment_status,
-    amount,
+    amount/100 as amount,
     created as created_at,
     _batched_at
 from raw.stripe.payment
