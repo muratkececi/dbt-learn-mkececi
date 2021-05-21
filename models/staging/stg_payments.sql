7222bd901946bcc0bdbc18b31f915b3064610d88
Murat Kececi <mkececi@conaservices.com>
2021-05-21 20:07:17 +0000

    added working group changes

--- models/staging/stg_payments.sql
+++ models/staging/stg_payments.sql
@@ -3,7 +3,7 @@ select
     orderid as order_id,
     paymentmethod as payment_method,
     status as payment_status,
-    amount/100 as amount,
+    {{ cents_to_dollars('amount') }} as amount,
     created as created_at,
     _batched_at
 from {{ source('raw_stripe', 'payment') }}
